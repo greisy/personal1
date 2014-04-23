f941ed19c1925f80bcc6d77a610964b06f466607
Grey <g.grey2411@gmail.com>
2014-04-22 21:54:31 -0430

    prueba de actualización de la aplicación hecha en rails Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+ # get "welcome/index"
+  resources :articles
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
