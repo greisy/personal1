47c0ec529d36ae2e903bea3f6b984bdade36e0c5
Grey <g.grey2411@gmail.com>
2014-04-25 21:28:27 -0430

    I don't remember what It's the number of this commit, but in this commit, I have added new data relate with the form of new articles, I created one controller with its view and the others files that it is generated when create a new controller.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController #Un controlador es una simple clase que hereda del ApplicationController
+#Dentro de esta clase se definen metodos que se convertiran en acciones de este controlador
+
+#Estas acciones actuaran como operaciones CRUD para el resource article
+
+  def new
+  end
+    
+
+end
