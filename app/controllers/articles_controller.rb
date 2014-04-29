81c6d08465f75c7be8bb6c84bc5e22a522548cd4
Grey <g.grey2411@gmail.com>
2014-04-28 23:01:02 -0430

    Today I made only test, I hope tomorrow doing more!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,17 @@ class ArticlesController < ApplicationController #Un controlador es una simple c
 
   def new #esta accion esta asociada a crear un nuevo article, su vista es el formulario para recolectar esta información y ser 
   	#dirigida a la acción create que se encuentra abajo
+  	render nothing: true
+  	
+
   end
 
   def create 									#es la acción asociada al momento en que se da submit al formulario de arriba y recibira la información para procesarla
-  	render plain: params[:article].inspect		#lo que la acción CREATE deberia hacer es guardar nuestro nuevo nuevo article en la BD
-  			 									#Cuando el formulario es enviado, los campos de este son enviados como parametros y pueden ser refrenciados dentro de la 
-  			 									#acción del controlador.
-
+  	#render plain: params[:article].inspect, layout:true		#lo que la acción CREATE deberia hacer es guardar nuestro nuevo nuevo article en la BD
+  			 									#Cuando el formulario es enviado, los campos de este son enviados como parametros y pueden ser referenciados dentro de las 
+  			 									#acciones del controlador.
+    #logger.debug "HELLO I AM A DEBUGGING STATEMENT"
+    #logger.debug "#{params[:article].inspect}"
   end
 
 
