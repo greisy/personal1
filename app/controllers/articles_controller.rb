c56d3ae6b3e187b7c7d38bcbbb3909fa8f7591e8
BunnyGrey <g.grey2411@gmail.com>
2014-05-01 18:46:05 -0430

    I have not done nothing else in my Rails project, only I have been checking the method render, even though I need to keep prove other things about this method and about Layout method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,20 +2,24 @@ class ArticlesController < ApplicationController #Un controlador es una simple c
 #Dentro de esta clase se definen metodos que se convertiran en acciones de este controlador
 
 #Estas acciones actuaran como operaciones CRUD para el resource article
-
+layout "capa"
   def new #esta accion esta asociada a crear un nuevo article, su vista es el formulario para recolectar esta información y ser 
   	#dirigida a la acción create que se encuentra abajo
-  	render nothing: true
-  	
-
+  	#render nothing: true, status: 404 #cuando comento esta instruccion Rails va a renderizar por defecto esta accion a través de su templates, sin embargo, si coloco esta instruccióm
+  	                       #le dará mayor prioridad a está instrucción para renderizarla (render nothing) 
+    
+  
   end
 
   def create 									#es la acción asociada al momento en que se da submit al formulario de arriba y recibira la información para procesarla
-  	#render plain: params[:article].inspect, layout:true		#lo que la acción CREATE deberia hacer es guardar nuestro nuevo nuevo article en la BD
-  			 									#Cuando el formulario es enviado, los campos de este son enviados como parametros y pueden ser referenciados dentro de las 
+  		#lo que la acción CREATE deberia hacer es guardar nuestro nuevo nuevo article en la BD
+  	#render plain: params[:article].inspect								#Cuando el formulario es enviado, los campos de este son enviados como parametros y pueden ser referenciados dentro de las 
   			 									#acciones del controlador.
     #logger.debug "HELLO I AM A DEBUGGING STATEMENT"
     #logger.debug "#{params[:article].inspect}"
+    #render text: params[:article].inspect
+    render plain: "<b>ESTO ES UNA PRUEBA</b>", layout: true, status: 200
+    #render body: params[:article].inspect
   end
 
 
