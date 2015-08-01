cc93c6b84c921e85a60f6f55b5aa29daedc4aa8f
Grey <g.grey2411@gmail.com>
2015-07-31 22:11:00 -0430

    ejercicios de HTML, CSS, JavaScript

---
+++ javascript/toggle.js
@@ -0,0 +1,23 @@
+/*$(document).ready(function() {
+	$('#toggle_message').click(function(){
+		var value = $('#toggle_message').attr('value');
+		$('#message').toggle('fast'); //averiguar esta parte
+
+		if(value == 'Hide'){
+			$('#toggle_message').attr('value','Show');
+		}else if (value=='Show') {
+			$('#toggle_message').attr('value', 'Hide');
+		}
+
+	});	 
+});*/
+
+$(document).ready(function(){
+
+	$('#nombre').attr('value', 'Greisy');
+	$('#cedula').attr('value', '1881361');
+	$('#telefono').value = '0212-9411252';
+
+
+
+});
